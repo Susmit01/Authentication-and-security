82f4afbf7b7ab21c6ecca2120af12027db580bbe
Susmita Mishra <heysusmita@gmail.com>
2020-09-15 12:24:02 +0530

    Bcrypt encryption

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const express= require("express");
 const bodyParser= require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
 const app= express();
+//md5 encryption-- const md5 = require("md5");
 
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+
+//bcrypt version-- salting
+const saltRounds= 3 //use more than 10 in real life
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,8 +23,8 @@ const userSchema= new mongoose.Schema ({
 	password: String
 });
 //encryption - secret
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//mongoose encryption
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User= new mongoose.model("User", userSchema);
 
@@ -32,10 +38,19 @@ app.get("/register", function(req, res){
 	res.render("register");
 });
 app.post("/register", function(req, res){
+	//bcrypt-
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+		const newUser = new User ({
+		email: req.body.username,
+		password: hash
+
+	});
+	/*md5--
 	const newUser = new User ({
 		email: req.body.username,
-		password: req.body.password
-	});
+		//md5-- password: md5(req.body.password)
+
+	});*/
 	newUser.save(function(err){
 		if (err){
 			console.log(err);
@@ -44,26 +59,30 @@ app.post("/register", function(req, res){
 		}
 	});
 });
+});
 app.post("/login", function(req, res){
 	const username = req.body.username;
+	//for md5- const password = md5(req.body.password);
+	//bcrypt
 	const password = req.body.password;
 	User.findOne({email: username}, function(err, foundUser){
 		if(err){
 			console.log(err);
 		} else{
 			if (foundUser){
-				if (foundUser.password === password){
-					res.render("secrets");
+				/*if (foundUser.password === password){
+					res.render("secrets");*/
+					//bcrypt--
+				bcrypt.compare(password, foundUser.password, function(err, result){
+						if(result == true){
+							res.render("secrets");
+						}
+				});
 				}
 			}
-		}
+		
 	});
 });
-
-
-
-
-
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
